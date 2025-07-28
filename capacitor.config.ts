920623f2ef6ef80a1d24a0cfce7a91ed6f74658e
lukasschick <schick_lukas@t-online.de>
2025-07-28 17:32:25 +0200

    Added splash for app starting screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'ScanManager',
-  webDir: 'dist'
+  webDir: 'dist',
+
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#ffffff",
+      androidSplashResourceName: "splash",
+      iosSplashResourceName: "splash",
+      showSpinner: false,
+      autoHide: true,
+    }
+  }
 };
 
 export default config;
